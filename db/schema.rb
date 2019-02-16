37e435ef3fe1b7190b30b3b36a35bb4002f1c932
Eric Florence <ericflorence.dev@gmail.com>
2019-02-16 11:10:09 -0800

    rollback migration to include trackables and redo migrate

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_02_16_190423) do
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
