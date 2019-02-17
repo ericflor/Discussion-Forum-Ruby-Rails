deffc00f65d9f612130ebdddcb8ee497b45fcdd9
Eric Florence <ericflorence.dev@gmail.com>
2019-02-17 12:23:02 -0800

    link up tables in models

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,2 +1,9 @@
 class Discussion < ApplicationRecord
+
+    belongs_to :channel
+    belongs_to :user
+    has_many :replies, dependent: :detroy
+
+    validates :title, :content, presence: true 
+
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,8 @@
 class Reply < ApplicationRecord
+
+    belongs_to :discussion
+    belongs_to :user
+
+    validtion :reply, presence: true
+
 end
